80f91217f1d5725c17f518b1b6015c8f51ae891b
chaumy791993 <chaumy791993@gmail.com>
2022-06-09 00:09:12 +0700

    up

--- ss1_typescript/bai_tap/typescript-fibonacci/src/main.js
+++ ss1_typescript/bai_tap/typescript-fibonacci/src/main.js
@@ -1,17 +1,30 @@
-function fibonacci(num) {
-    var n1 = 0;
-    var n2 = 1;
-    var n3;
-    var list = [];
-    var tong = 0;
-    for (var i = 1; i <= num; i++) {
-        n3 = n1 + n2;
-        n1 = n2;
-        n2 = n3;
-        list.push(n1);
-        tong += n1;
+var n1 = 0;
+var n2 = 1;
+var n3;
+var list = [];
+var sum = 0;
+function fibonacci(total, count) {
+    if (count < total) {
+        if (count <= 1) {
+            n3 = count;
+        }
+        else {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        count++;
+        list.push(n3);
+        fibonacci(total, count);
     }
-    console.log(list);
-    console.log(tong);
+    else {
+        return list;
+    }
 }
-fibonacci(20);
+fibonacci(10, 0);
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var number = list_1[_i];
+    sum += number;
+}
+console.log(list);
+console.log(sum);
--- ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
+++ ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
@@ -1,19 +1,30 @@
 let n1: number = 0;
 let n2: number = 1;
 let n3: number;
-let list:Array<number>=[];
-let sum :number= 0;
-function fibonacci(num:number) :void {
-    let size = num
-    if (size > 0){
-        n3 = n1 + n2;
-        n1 = n2;
-        n2 = n3;
-        list.push(n1);
-        sum += n1;
+let list: number[] = [];
+let sum: number = 0;
+
+function fibonacci(total: number, count: number): Array<number> {
+    if (count < total) {
+        if (count <= 1) {
+            n3 = count;
+        } else {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        count++;
+        list.push(n3);
+        fibonacci(total,count);
+    }else {
+        return list;
     }
-
-
-    console.log(list);
-    console.log(sum);
 }
+fibonacci(10,0)
+
+for (let number of list){
+    sum += number;
+}
+
+console.log(list);
+console.log(sum);
