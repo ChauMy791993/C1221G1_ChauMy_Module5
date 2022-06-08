76318857108ec5be5310927e9d22f72d34a42233
chaumy791993 <chaumy791993@gmail.com>
2022-06-08 08:51:42 +0700

    up

--- ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
+++ ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
@@ -1,13 +1,13 @@
 let n1: number = 0;
 let n2: number = 1;
 let total: number;
-let list = "";
+let list:number[];
 let tong :number= 0;
 for (let i = 1; i <= 20; i++) {
     total = n1 + n2;
     n1 = n2;
     n2 = total;
-    list += +n1 + ",";
+    list.push(n1);
     tong += n1;
 }
 console.log(list)
