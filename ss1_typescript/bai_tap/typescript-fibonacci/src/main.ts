6f5a108eab79b42cfe0fd65b543c0f4f56ea3c75
chaumy791993 <chaumy791993@gmail.com>
2022-06-08 18:53:33 +0700

    up

--- ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
+++ ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
@@ -1,17 +1,19 @@
+let n1: number = 0;
+let n2: number = 1;
+let n3: number;
+let list:Array<number>=[];
+let sum :number= 0;
 function fibonacci(num:number) :void {
-    let n1: number = 0;
-    let n2: number = 1;
-    let n3: number;
-    let list:Array<number>=[];
-    let tong :number= 0;
-    for (let i = 1; i <= num; i++) {
+    let size = num
+    if (size > 0){
         n3 = n1 + n2;
         n1 = n2;
         n2 = n3;
         list.push(n1);
-        tong += n1;
+        sum += n1;
     }
+
+
     console.log(list);
-    console.log(tong);
+    console.log(sum);
 }
-fibonacci(20);
