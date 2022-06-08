77fcbdb2081a128daf65cb3f5fc72f5812042911
chaumy791993 <chaumy791993@gmail.com>
2022-06-08 09:32:08 +0700

    up

--- ss1_typescript/bai_tap/typescript-fibonacci/src/main.js
+++ ss1_typescript/bai_tap/typescript-fibonacci/src/main.js
@@ -1,14 +1,17 @@
-var n1 = 0;
-var n2 = 1;
-var total;
-var list = "";
-var tong = 0;
-for (var i = 1; i <= 20; i++) {
-    total = n1 + n2;
-    n1 = n2;
-    n2 = total;
-    list += +n1 + ",";
-    tong += n1;
+function fibonacci(num) {
+    var n1 = 0;
+    var n2 = 1;
+    var n3;
+    var list = [];
+    var tong = 0;
+    for (var i = 1; i <= num; i++) {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        list.push(n1);
+        tong += n1;
+    }
+    console.log(list);
+    console.log(tong);
 }
-console.log(list);
-console.log(tong);
+fibonacci(20);
--- ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
+++ ss1_typescript/bai_tap/typescript-fibonacci/src/main.ts
@@ -1,14 +1,17 @@
-let n1: number = 0;
-let n2: number = 1;
-let total: number;
-let list:number[];
-let tong :number= 0;
-for (let i = 1; i <= 20; i++) {
-    total = n1 + n2;
-    n1 = n2;
-    n2 = total;
-    list.push(n1);
-    tong += n1;
+function fibonacci(num:number) :void {
+    let n1: number = 0;
+    let n2: number = 1;
+    let n3: number;
+    let list:Array<number>=[];
+    let tong :number= 0;
+    for (let i = 1; i <= num; i++) {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        list.push(n1);
+        tong += n1;
+    }
+    console.log(list);
+    console.log(tong);
 }
-console.log(list)
-console.log(tong);
+fibonacci(20);
