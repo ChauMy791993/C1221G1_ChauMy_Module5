0b4477bb0ca1abe7573b6e56ff6bdf3cb41257b5
chaumy791993 <chaumy791993@gmail.com>
2022-06-09 22:20:39 +0700

    up

--- case_study/angular-furama-managament/src/app/app.module.ts
+++ case_study/angular-furama-managament/src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { FacilityComponent } from './facility/facility.component';
-import { FacilityCreateComponent } from './facility-create/facility-create.component';
-import { FacilityEditComponent } from './facility-edit/facility-edit.component';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerCreateComponent } from './customer-create/customer-create.component';
-import { CustomerEditComponent } from './customer-edit/customer-edit.component';
-import { ContractComponent } from './contract/contract.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
+import {FacilityComponent} from './facility/facility.component';
+import {FacilityCreateComponent} from './facility-create/facility-create.component';
+import {FacilityEditComponent} from './facility-edit/facility-edit.component';
+import {CustomerComponent} from './customer/customer.component';
+import {CustomerCreateComponent} from './customer-create/customer-create.component';
+import {CustomerEditComponent} from './customer-edit/customer-edit.component';
+import {ContractComponent} from './contract/contract.component';
+import {HomeComponent} from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ContractCreateComponent } from './contract-create/contract-create.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'contract', component: ContractComponent},
+  {path: 'contract/create', component: ContractCreateComponent},
+  {path: 'customer' , component: CustomerComponent},
+  {path: 'customer/create' , component: CustomerCreateComponent},
+  {path: 'customer/edit/:customerId', component: CustomerEditComponent},
+  {path: 'facility' , component: FacilityComponent},
+  {path: 'facility/create' , component: FacilityCreateComponent},
+  {path: 'facility/edit/:facilityId' , component: FacilityEditComponent},
+];
 
 @NgModule({
   declarations: [
@@ -25,12 +39,15 @@ import { HomeComponent } from './home/home.component';
     CustomerCreateComponent,
     CustomerEditComponent,
     ContractComponent,
-    HomeComponent
+    HomeComponent,
+    ContractCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
